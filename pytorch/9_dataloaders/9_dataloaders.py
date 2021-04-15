f421f939968a4a574bd0df15946d6c2ea02ab9bc
tung.hsiao <tung.hsiao@xiaotongs-MacBook-Air.local>
2021-04-15 17:20:13 +0800

    update pytorch dataloaders

--- pytorch/9_dataloaders/9_dataloaders.py
+++ pytorch/9_dataloaders/9_dataloaders.py
@@ -47,7 +47,7 @@ plt.figure()
 show_landmarks(io.imread(os.path.join("/content/faces/", img_name)),
                landmarks)
 
-# PART 3: 
+# PART 3: create a custom dataset class
 # ==================================
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms, utils
@@ -59,7 +59,9 @@ from torchvision import transforms, utils
 # read the csv in __init__ but leave the reading of images to __getitem__. 
 # This is memory efficient
 
-class FaceLandMarksDataset(Dataset):
+import torch
+
+class FaceLandmarksDataset(Dataset):
   def __init__(self, csv_file, root_dir, transform=None):
     self.landmarks_frame = pd.read_csv(csv_file)
     self.root_dir = root_dir
@@ -69,4 +71,124 @@ class FaceLandMarksDataset(Dataset):
     return len(self.landmarks_frame)
   
   def __getitem__(self, idx):
-    pass
+    if torch.is_tensor(idx):
+      idx = idx.tolist()
+    
+    img_name = os.path.join(self.root_dir, self.landmarks_frame.iloc[idx, 0])
+    image = io.imread(img_name)
+    landmarks = self.landmarks_frame.iloc[idx,1:]
+    landmarks = np.array([landmarks])
+    landmarks = landmarks.astype("float").reshape(-1,2)
+    sample = {"image": image, "landmarks": landmarks}
+
+    if self.transform:
+      sample = self.transform(sample)
+    return sample
+
+face_dataset = FaceLandmarksDataset(csv_file="/content/faces/face_landmarks.csv",
+                                    root_dir="/content/faces/")
+for i in range(3):
+    sample = face_dataset[i]   # sample is a dict: {"image": image, "landmarks": landmarks}
+    print("Sample#", i, "image shape:", sample['image'].shape, "landmarks shape:",sample['landmarks'].shape)
+    show_landmarks(**sample)   # equal to: show_landmarks(sample['image'], sample["landmarks"])
+
+# PART 4: define Transforms
+# ==================================
+# Rescale: resize the image
+# RandomCrop: it's for data augmentation
+# ToTensor: convert numpy image to torch image (swap axes)
+
+class Rescale(object):
+  def __init__(self, output_size):
+    assert isinstance(output_size, (int, tuple))
+    self.output_size = output_size
+  
+  def __call__(self, sample):
+    image, landmarks = sample["image"], sample["landmarks"]
+
+    h,w = image.shape[:2]
+    if isinstance(self.output_size, int):           # int
+      if h>w:
+        new_h, new_w = self.output_size * h / w, self.output_size
+      else:
+        new_h, new_w = self.output_size, self.output_size * w / h
+    else:                                           # tuple
+      new_h, new_w = self.output_size
+    
+    new_h, new_w = int(new_h), int(new_w)           # Integer argument required for resize
+    img = transform.resize(image, (new_h, new_w))
+    landmarks = landmarks * [new_w/w, new_h/h]
+
+    return {"image":img, "landmarks":landmarks}
+
+class RandomCrop(object):
+  def __init__(self, output_size):
+    assert isinstance(output_size, (int, tuple))
+    if isinstance(output_size, int):                # int
+      self.output_size= (output_size, output_size)
+    else:
+      assert len(output_size) == 2                  # tuple
+      self.output_size = output_size
+  
+  def __call__(self, sample):
+    image, landmarks = sample["image"], sample["landmarks"]
+
+    h,w = image.shape[:2]
+    new_h, new_w = self.output_size
+
+    top = np.random.randint(0, h-new_h)
+    left = np.random.randint(0, w-new_w)
+
+    image = image[top:top+new_h, left:left+new_w]
+    landmarks = landmarks - [left, top]
+    return {"image": image, "landmarks": landmarks}
+
+class ToTensor(object):
+  def __call__(self, sample):
+    image, landmarks = sample["image"], sample["landmarks"]
+
+    # numpy image: H x W x C
+    # torch image: C x H x W
+    image = image.transpose((2,0,1))
+    return {"image": torch.from_numpy(image), 
+            "landmarks": torch.from_numpy(landmarks)}
+
+composed = transforms.Compose([Rescale(256),
+                               RandomCrop(224)])
+
+# sample = face_dataset[65]
+# scale = Rescale(256)
+# transformed_sample = scale(sample)
+# show_landmarks(**transformed_sample)
+
+# crop = RandomCrop(128)
+# transformed_sample = crop(sample)
+# show_landmarks(**transformed_sample)
+
+# PART 5: iterating transforms through the dataset
+# ==================================
+transformed_dataset = FaceLandmarksDataset(csv_file="/content/faces/face_landmarks.csv",
+                                    root_dir="/content/faces/",
+                                    transform=transforms.Compose([
+                                               Rescale(256),
+                                               RandomCrop(224),
+                                               ToTensor()
+                                               ]))
+i = 0
+sample = transformed_dataset[i]
+print(i, "image shape:", sample['image'].shape, "landmarks shape:",sample['landmarks'].shape)
+# 0 image shape: torch.Size([3, 224, 224]) landmarks shape: torch.Size([68, 2])
+
+# PART 6: dataloader
+# ==================================
+
+dataloader = DataLoader(transformed_dataset, batch_size=4, shuffle=True, num_workers=0)
+# num_workers (int, optional): how many subprocesses to use for data 
+# loading. 0 means that the data will be loaded in the main process. (default: 0)
+
+for i_batch, sample_batched in enumerate(dataloader):
+  print(i_batch, sample_batched['image'].size(), sample_batched['landmarks'].size())
+  # 0 torch.Size([4, 3, 224, 224]) torch.Size([4, 68, 2])
+  
+  if i_batch == 2:
+    break
